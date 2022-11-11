086f461ba6e8df1f762b507911bcf89f7c31af9d
Andrey Mattos <andreysmattos@hotmail.com>
2022-11-11 07:55:21 -0300

    products page

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-box',
   templateUrl: './product-box.component.html',
 })
 export class ProductBoxComponent implements OnInit {
+  
+  @Input() fullWidthMode = false;
 
   constructor() { }
 
