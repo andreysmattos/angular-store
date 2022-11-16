2ef01c509c8fcf303340bc56dbcd8a8a4ce3c446
Andrey Mattos <andreysmattos@hotmail.com>
2022-11-16 08:19:18 -0300

    cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -18,6 +18,15 @@ export class CartComponent implements OnInit {
         price: 150,
         quantity: 1,
       },
+      {
+        id: 2,
+
+        name: 'Book',
+        product: 'https://via.placeholder.com/150',
+
+        price: 35,
+        quantity: 2,
+      },
     ]
   }
 
@@ -26,16 +35,20 @@ export class CartComponent implements OnInit {
   displayedColumuns: string[] = [
     'product',
     'name',
-    // 'price',
-    // 'quantity',
-    // 'total',
-    // 'action'
+    'price',
+    'quantity',
+    'total',
+    'action'
   ];
 
   constructor() { }
 
   ngOnInit(): void {
     this.dataSource = this.cart.items
+  }
+
+  getTotal(items: CartItem[]): number {
+    return items.map(item => item.price * item.quantity).reduce((prev,current) => prev+current, 0);
   }
 
 }
